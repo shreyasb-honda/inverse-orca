9eb379f41f47fbc97ba49bac3061982f3030df9b
Shreyas Bhat <shreyas_bhat@honda-ri.com>
2024-06-14 16:28:54 -0700

    Added one test. Passes it. Needs more testing

--- policy/utils/estimate_alpha.py
+++ policy/utils/estimate_alpha.py
@@ -9,6 +9,10 @@ from policy.utils.overlap_detection import Point
 
 def estimate_alpha(v_pref: Point, vA_new: Point, vA_new_exp: Point,
                    alpha_hat: float):
+    vA_new = tuple(np.round(vA_new, 4))
+    vA_new_exp = tuple(np.round(vA_new_exp, 4))
+    v_pref = tuple(np.round(v_pref, 4))
+
     if vA_new == v_pref == vA_new_exp:
     # Case A.1 or B.1: we do not get any new information about alpha
         print("Case A.1 or Case B.1")
@@ -32,5 +36,6 @@ def estimate_alpha(v_pref: Point, vA_new: Point, vA_new_exp: Point,
     print("Case A.2 or B.2")
     d1 = norm(np.array(vA_new) - np.array(v_pref))
     d2 = norm(np.array(vA_new_exp) - np.array(v_pref))
+
 
     return alpha_hat + d1 - d2
--- tests/estimate_alpha_testing.py
+++ tests/estimate_alpha_testing.py
@@ -15,11 +15,11 @@ RADIUS_A = 0.3
 RADIUS_B = 0.3
 VB_MAX = 1.0
 
-def test(relative_position: Point, vA: Point, v_pref: Point, 
+def test(relative_position: Point, vA: Point, v_pref: Point,
          vB: Point, alpha_hat: float, alpha: float):
 
     params = (10., 10, TAU, 2.0)
-    sim1 = rvo2.PyRVOSimulator(1.0, *params, RADIUS_B, VB_MAX)
+    sim1 = rvo2.PyRVOSimulator(0.25, *params, RADIUS_B, VB_MAX)
     sim1.addAgent(relative_position, *params, RADIUS_B, VB_MAX, vB, collisionResponsibility=1.0)
     sim1.addAgent((0., 0.), *params, RADIUS_A, VB_MAX, vA, collisionResponsibility=alpha_hat)
     sim1.setAgentPrefVelocity(0, vB)
@@ -30,13 +30,18 @@ def test(relative_position: Point, vA: Point, v_pref: Point,
     vA_new_exp = sim1.getAgentVelocity(1)
 
     params = (10., 10, TAU, 2.0)
-    sim2 = rvo2.PyRVOSimulator(1.0, *params, RADIUS_B, VB_MAX)
+    sim2 = rvo2.PyRVOSimulator(0.25, *params, RADIUS_B, VB_MAX)
     sim2.addAgent(relative_position, *params, RADIUS_B, VB_MAX, vB, collisionResponsibility=1.0)
     sim2.addAgent((0., 0.), *params, RADIUS_A, VB_MAX, vA, collisionResponsibility=alpha)
     sim2.setAgentPrefVelocity(0, vB)
     sim2.setAgentPrefVelocity(1, v_pref)
 
-    vA_new = sim1.getAgentVelocity(1)
+    sim2.doStep()
+
+    vA_new = sim2.getAgentVelocity(1)
+
+    print(f"vA_new {vA_new}")
+    print(f"vA_new_exp {vA_new_exp}")
 
     alpha_hat_new = estimate_alpha(v_pref, vA_new, vA_new_exp, alpha_hat)
 
@@ -47,7 +52,14 @@ def test(relative_position: Point, vA: Point, v_pref: Point,
 def test_case_A_1():
     relative_position = (1., 1.)
     v_pref = (-1.0, 0)
-    
+    alpha_hat = 0.9
+    alpha = 0.8
+
+    vA = (0.5, 0)
+    vB = (0, -0.4)
+
+    test(relative_position, vA, v_pref, vB, alpha_hat, alpha)
+
 
 def test_case_A_2():
     pass
