db580877957fbb36adf4af3722c2812f00c539fb
Shreyas Bhat <shreyas_bhat@honda-ri.com>
2024-06-14 13:01:28 -0700

    Added code to estimate alpha. To do: Add tests and debug

---
+++ policy/utils/estimate_alpha.py
@@ -0,0 +1,36 @@
+"""
+Given the preferred velocity, the estimated new velocity, and the actual new velocity, 
+estimate the value of the collision avoidance responsibility (alpha) of the agent. 
+This currently assumes that the robot knows the preferred velocity of the human
+"""
+import numpy as np
+from numpy.linalg import norm
+from policy.utils.overlap_detection import Point
+
+def estimate_alpha(v_pref: Point, vA_new: Point, vA_new_exp: Point,
+                   alpha_hat: float):
+    if vA_new == v_pref == vA_new_exp:
+    # Case A.1 or B.1: we do not get any new information about alpha
+        print("Case A.1 or Case B.1")
+        return alpha_hat
+
+    if vA_new == v_pref != vA_new_exp:
+        # Case A.3: our estimate of alpha is higher than the actual
+        # Reduce it
+        print("Case A.3")
+        reduction = norm(np.array(vA_new_exp) - np.array(v_pref))
+        return alpha_hat - reduction
+
+    if vA_new != v_pref == vA_new_exp:
+        # Case B.3: our estimate of alpha is lower than the actual
+        # Increase it
+        print("Case B.3")
+        increase = norm(np.array(vA_new) - np.array(v_pref))
+        return alpha_hat + increase
+
+    # Case A.2 or B.2: None of them are equal
+    print("Case A.2 or B.2")
+    d1 = norm(np.array(vA_new) - np.array(v_pref))
+    d2 = norm(np.array(vA_new_exp) - np.array(v_pref))
+
+    return alpha_hat + d1 - d2
---
+++ tests/estimate_alpha_testing.py
@@ -0,0 +1,65 @@
+"""
+Test the estimation of the collision avoidance responsibility. 
+Set relative_position, vA, v_pref, vB, alpha_hat to some values
+Set some true alpha
+Create two PyRVOSimulators, one with alpha_hat and the other with alpha
+do a step of the simulation. get vA_new_exp with alpha_hat and vA_new with alpha.
+Supply these values to the estimator and check the result
+"""
+import rvo2
+from policy.utils.overlap_detection import Point
+from policy.utils.estimate_alpha import estimate_alpha
+
+TAU = 6
+RADIUS_A = 0.3
+RADIUS_B = 0.3
+VB_MAX = 1.0
+
+def test(relative_position: Point, vA: Point, v_pref: Point, 
+         vB: Point, alpha_hat: float, alpha: float):
+
+    params = (10., 10, TAU, 2.0)
+    sim1 = rvo2.PyRVOSimulator(1.0, *params, RADIUS_B, VB_MAX)
+    sim1.addAgent(relative_position, *params, RADIUS_B, VB_MAX, vB, collisionResponsibility=1.0)
+    sim1.addAgent((0., 0.), *params, RADIUS_A, VB_MAX, vA, collisionResponsibility=alpha_hat)
+    sim1.setAgentPrefVelocity(0, vB)
+    sim1.setAgentPrefVelocity(1, v_pref)
+
+    sim1.doStep()
+
+    vA_new_exp = sim1.getAgentVelocity(1)
+
+    params = (10., 10, TAU, 2.0)
+    sim2 = rvo2.PyRVOSimulator(1.0, *params, RADIUS_B, VB_MAX)
+    sim2.addAgent(relative_position, *params, RADIUS_B, VB_MAX, vB, collisionResponsibility=1.0)
+    sim2.addAgent((0., 0.), *params, RADIUS_A, VB_MAX, vA, collisionResponsibility=alpha)
+    sim2.setAgentPrefVelocity(0, vB)
+    sim2.setAgentPrefVelocity(1, v_pref)
+
+    vA_new = sim1.getAgentVelocity(1)
+
+    alpha_hat_new = estimate_alpha(v_pref, vA_new, vA_new_exp, alpha_hat)
+
+    print("Old estimate of alpha:", alpha_hat)
+    print("New estimate of alpha:", alpha_hat_new)
+    print("True value of alpha:  ", alpha)
+
+def test_case_A_1():
+    relative_position = (1., 1.)
+    v_pref = (-1.0, 0)
+    
+
+def test_case_A_2():
+    pass
+
+def test_case_A_3():
+    pass
+
+def test_case_B_1():
+    pass
+
+def test_case_B_2():
+    pass
+
+def test_case_B_3():
+    pass
