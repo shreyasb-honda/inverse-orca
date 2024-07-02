1effc076b734c6a38e78bba92c5276c8093b0623
Shreyas Bhat <shreyas_bhat@honda-ri.com>
2024-07-02 16:25:41 -0700

    Added plotting features to data explorer

--- data_explorer.py
+++ data_explorer.py
@@ -2,10 +2,13 @@
 Classes for reading the stored simulation data
 """
 import os
+from math import sqrt
 import pickle
 import toml
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sim.renderer import Renderer
 sns.set_theme(context='talk')
 
 
@@ -14,22 +17,24 @@ class DataReader:
     Takes a parent directory and reads the relevant information from it
     """
 
-    def __init__(self, parent_dir: str) -> None:
+    def __init__(self, parent_dir: str, indep_var: str) -> None:
         self.parent_dir = parent_dir
         self.data = {}
-        self.indep_var = None
+        self.indep_var = indep_var
+        self.env_config = None
+        self.policy_config = None
 
-    def read_data(self, indep_var: str):
+    def read_data(self):
         """
         Reads and stores the data
         """
-        self.indep_var = indep_var
+        indep_var = self.indep_var
         experiments = os.listdir(self.parent_dir)  # Will contain one folder for each experiment
 
         for experiment in experiments:
             # Load the configuration
             env_config = toml.load(os.path.join(self.parent_dir, experiment, 'env.toml'))
-            sim_config = toml.load(os.path.join(self.parent_dir, experiment, 'sim.toml'))
+            # sim_config = toml.load(os.path.join(self.parent_dir, experiment, 'sim.toml'))
             policy_config = toml.load(os.path.join(self.parent_dir, experiment, 'policy.toml'))
             if indep_var == "alpha":
                 alpha = env_config['human']['collision_responsibility']
@@ -67,7 +72,6 @@ class DataReader:
         ax.set_xlabel(self.indep_var)
 
         return fig, ax
-
 
     def plot(self):
         """
@@ -107,7 +111,6 @@ class DataReader:
 
         plt.show()
 
-
     def print(self):
         """
         Prints the metrics to stdout
@@ -119,19 +122,133 @@ class DataReader:
                 val2_s = f"{perf_summary['std'][key]:.3f}"
                 print(f"{key:<35}: Average {val_s:<10} Std {val2_s:<10}")
 
+    def plot_random_trajectory(self, indep_var_val):
+        """
+        Plots a random trajectory for the given value
+        of the independent variable
+        :param indep_var_value - the value of the independent variable for which 
+                                 a random trajectory is to be plotted
+        """
+        experiments = os.listdir(self.parent_dir)
+        for experiment in experiments:
+            env_config = toml.load(os.path.join(self.parent_dir, experiment, 'env.toml'))
+            # sim_config = toml.load(os.path.join(self.parent_dir, experiment, 'sim.toml'))
+            policy_config = toml.load(os.path.join(self.parent_dir, experiment, 'policy.toml'))
+            if self.indep_var == "alpha":
+                val = env_config['human']['collision_responsibility']
+            elif self.indep_var == 'max_speed':
+                val = env_config['robot']['max_speed']
+            elif self.indep_var == 'time_horizon':
+                val = policy_config['inverse_orca']['time_horizon']
+            if val == indep_var_val:
+                chosen_exp = experiment
+                break
+
+        # Grab a random folder from the subfolders
+        runs = os.listdir(os.path.join(self.parent_dir, chosen_exp))
+        chosen_run = np.random.choice(runs)
+        run_dir = os.path.join(self.parent_dir, chosen_exp, chosen_run)
+        print(run_dir)
+        with open(os.path.join(run_dir, 'obs.pkl'), 'rb') as f:
+            observations = pickle.load(f)
+
+        hallway_length = env_config['env']['hallway_length']
+        hallway_width = env_config['env']['hallway_width']
+        robot_draw_params = {"radius": observations[0]['robot rad'], "color": "tab:blue"}
+        human_draw_params = {"radius": observations[0]['human rad'], "color": 'tab:red'}
+        hallway_dimensions = {"length": hallway_length, "width": hallway_width}
+        renderer = Renderer(hallway_dimensions, robot_draw_params, human_draw_params,
+                            env_config['env']['time_step'])
+        d_virtual_goal = env_config['env']['d_virtual_goal']
+        y_virtual_goal = env_config['env']['y_virtual_goal']
+        renderer.set_goal_params(d_virtual_goal, y_virtual_goal)
+
+        goal_frame = self.get_goal_frame(observations, env_config)
+        renderer.set_observations(observations, goal_frame)
+        fig, ax = plt.subplots()
+        ax.set_aspect('equal')
+        renderer.static_plot(fig, ax)
+
+    def get_goal_frame(self, observations, env_config):
+        """
+        Returns the frame number of the frame in which 
+        the human reaches the virtual goal line
+        """
+        frame = 0
+        for obs in observations:
+            if obs['human pos'][1] <= env_config['env']['y_virtual_goal']:
+                break
+            frame += 1
+        return frame
+
+
 def main():
     """
     The main function
     """
-    # parent_dir = os.path.join('data', 'human-orca-robot-inverse', 'alpha_effect')
-    # parent_dir = os.path.join('data', 'human-orca-robot-inverse', 'max_speed_effect')
-    parent_dir = os.path.join('data', 'human-orca-robot-inverse', 'time_horizon_effect')
+    policy_combination = ['orca', 'weighted']
+    indep_var = 'alpha'
+    parent_dir = os.path.join('data',
+                              f'human-{policy_combination[0]}-robot-{policy_combination[1]}',
+                              'weight-0.2',
+                              f'{indep_var}_effect')
 
-    data_explorer = DataReader(parent_dir=parent_dir)
+    data_explorer = DataReader(parent_dir=parent_dir, indep_var=indep_var)
 
-    data_explorer.read_data('time_horizon')
-    data_explorer.plot()
+    data_explorer.read_data()
+    # data_explorer.plot()
+    data_explorer.plot_random_trajectory(0.5)
+    plt.show()
 
+
+def time_horizon_effect():
+    """
+    Plots the data from the independent variation of the planning time
+    horizons for the human and the robot
+    For each pair of (tau_human, tau_robot), we have one value of the performance metric
+    We can show the plots as an image (plt.imshow)
+    """
+    parent_dir = os.path.join('data', 'human-orca-robot-inverse', 'both_time_horizons')
+    experiments = os.listdir(parent_dir)
+    data = {}
+    metric_names = None
+    for experiment in experiments:
+        policy_config = toml.load(os.path.join(parent_dir, experiment, 'policy.toml'))
+        tau_robot = policy_config['inverse_orca']['time_horizon']
+        tau_human = policy_config['orca']['time_horizon']
+        perf_file = os.path.join(parent_dir, experiment, 'perf_summary.pkl')
+        with open(perf_file, 'rb') as f:
+            perf_summary = pickle.load(f)
+        avg_perf = perf_summary['mean']
+        if metric_names is None:
+            metric_names = list(avg_perf.keys())
+        data[(tau_human, tau_robot)] = avg_perf
+
+    num_data_points = len(data.keys())
+    num_rows = int(sqrt(num_data_points))
+    for metric_name in metric_names:
+        data_array = np.zeros((num_rows, num_rows))
+        for (human_tau, robot_tau), val in data.items():
+            idx_1 = (human_tau - 2) // 2
+            idx_2 = (robot_tau - 2) // 2
+            data_array[idx_1, idx_2] = val[metric_name]
+
+        fig, ax = plt.subplots()
+        im = ax.imshow(data_array, origin='lower')
+        ax.set_title(metric_name)
+        ax.set_xlabel('robot tau')
+        ax.set_ylabel('human tau')
+        cbar = ax.figure.colorbar(im, ax=ax)
+
+        col_labels = [2*(i+1) for i in range(7)]
+        ax.set_xticks(np.arange(data_array.shape[1]), labels=col_labels)
+        ax.set_yticks(np.arange(data_array.shape[0]), labels=col_labels)
+        ax.grid(False)
+
+        fig.tight_layout()
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
+    # time_horizon_effect()
--- time_horizon_effect.py
+++ time_horizon_effect.py
@@ -3,23 +3,18 @@ Runs the code to check the effect of differing planning horizon
 for the robot and the human
 """
 import os
+from typing import List, Dict
 from run_sim import SimulationRunner
 
+def only_robot(time_horizons: List[int], 
+               env_config: str, sim_config: str, policy_config: str):
 
-def main():
     """
-    Just the main function
+    Run simulations by varying the robot's planning time horizon
     """
-
-    # Config files
-    config_directory = os.path.join('sim', 'config')
-    env_config = os.path.join(config_directory, 'env.toml')
-    sim_config = os.path.join(config_directory, 'sim.toml')
-    policy_config = os.path.join(config_directory, 'policy.toml')
-    time_horizons = [i+1 for i in range(10)]
 
     for tau in time_horizons:
-        print("Robot Tau:", tau)
+        # print("Robot Tau:", tau)
         sim_runner = SimulationRunner()
         # configure the simulation runner
         sim_runner.configure_from_file(sim_config=sim_config,
@@ -29,6 +24,52 @@ def main():
         sim_runner.config['policy']['inverse_orca']['time_horizon'] = tau
         sim_runner.setup_environment()
         sim_runner.run_sim()
+
+def robot_and_human(time_horizons: Dict[str, List[int]],
+                    env_config: str, sim_config: str, policy_config: str):
+    """
+    Change both the human's and the robot's planning horizons
+    """
+    list1 = time_horizons['human']
+    list2 = time_horizons['robot']
+
+    for tau1 in list1:
+        for tau2 in list2:
+            sim_runner = SimulationRunner()
+            # configure the simulation runner
+            sim_runner.configure_from_file(sim_config=sim_config,
+                                        env_config=env_config,
+                                        policy_config=policy_config)
+            sim_runner.config['sim']['render_mode'] = None
+            sim_runner.config['policy']['orca']['time_horizon'] = tau1
+            sim_runner.config['policy']['inverse_orca']['time_horizon'] = tau2
+            sim_runner.setup_environment()
+            sim_runner.run_sim()
+
+
+def main():
+    """
+    Just the main function
+    """
+    # Config files
+    config_directory = os.path.join('sim', 'config')
+    env_config = os.path.join(config_directory, 'env.toml')
+    sim_config = os.path.join(config_directory, 'sim.toml')
+    policy_config = os.path.join(config_directory, 'policy.toml')
+
+    # Only change the robot's planning time horizon
+    # time_horizons = [i+1 for i in range(10)]
+    # only_robot(time_horizons, env_config, sim_config, policy_config)
+
+    # Change the robot's and the human's planning time horizons
+    time_horizons = {'human': [], 'robot': []}
+    tau_list = [2*(i+1) for i in range(7)]
+    for tau in tau_list:
+        time_horizons['human'].append(tau)
+        time_horizons['robot'].append(tau)
+
+    robot_and_human(time_horizons, env_config, sim_config, policy_config)
+
 
 if __name__ == "__main__":
     main()
