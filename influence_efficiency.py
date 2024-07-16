Fixed a bug in the dmax computation in get_velocity.py

influence_efficiency.py
@@ -13,17 +13,19 @@ logging.disable(logging.ERROR)
 
 HUMAN_POS = (0., 0.)
 HUMAN_HEADING = (-1., 0.)
-DESIRED_HEADING = np.array([-2., 1.])
+DESIRED_HEADING = np.array([-1., -1.])
 DESIRED_HEADING = tuple(DESIRED_HEADING / norm(DESIRED_HEADING))
 AGENT_RADIUS = 0.3
 MAX_RADIUS = 3
 TIME_HORIZON = 6
 NUM_SAMPLES = int(1e4)
-MAX_SPEED = 1.5
+MAX_SPEED = 2.0
 HUMAN_CIRCLE = Circle(HUMAN_POS, AGENT_RADIUS)
 
 HUMAN_POLICY = 'orca'
 # HUMAN_POLICY = 'social_force'
+
+VMIN = np.dot(HUMAN_HEADING, DESIRED_HEADING)
 
 
 def main():
@@ -33,7 +35,7 @@ def main():
     rng = np.random.default_rng(seed=123)
     i = 0
 
-    data = np.zeros((NUM_SAMPLES, 10))
+    data = np.zeros((NUM_SAMPLES, 12))
     while i < NUM_SAMPLES:
         theta = 2 * np.pi * rng.random()
         radius = MAX_RADIUS * rng.random()
@@ -104,6 +106,7 @@ def main():
             vh = np.array([states[1, 0, 2], states[1, 0, 3]])
 
         dot = np.dot(vh, DESIRED_HEADING)
+        expected_dot = np.dot(opt.vh_new, DESIRED_HEADING)
         # Save the data
         data[i, 0] = theta
         data[i, 1] = radius
@@ -115,12 +118,15 @@ def main():
         data[i, 7] = vh[0]
         data[i, 8] = vh[1]
         data[i, 9] = dot
+        data[i, 10] = expected_dot / norm(vh)
+        data[i, 11] = expected_dot
 
         i += 1
 
     print(data[:, 4].max())
     fig, ax = plt.subplots()
-    scatter = ax.scatter(x=data[:, 2], y=data[:, 3], c=data[:, 4], cmap='viridis', vmin=0.0, vmax=1.0)
+    scatter = ax.scatter(x=data[:, 2], y=data[:, 3],
+                         c=data[:, 4], cmap='viridis', vmin=VMIN, vmax=1.0)
 
     ax.arrow(HUMAN_POS[0], HUMAN_POS[1],
              HUMAN_HEADING[0], HUMAN_HEADING[1],
@@ -133,8 +139,25 @@ def main():
     ax.legend(loc='upper right')
     plt.colorbar(scatter)
 
+
     fig, ax = plt.subplots()
-    scatter = ax.scatter(x=data[:, 2], y=data[:, 3], c=data[:, 9], cmap='viridis', vmin=0.0, vmax=1.0)
+    scatter = ax.scatter(x=data[:, 2], y=data[:, 3],
+                         c=data[:, 10], cmap='viridis', vmin=VMIN, vmax=1.0)
+
+    ax.arrow(HUMAN_POS[0], HUMAN_POS[1],
+             HUMAN_HEADING[0], HUMAN_HEADING[1],
+             color='red', lw=1.5, label=r'$v_h$')
+    ax.arrow(HUMAN_POS[0], HUMAN_POS[1],
+             DESIRED_HEADING[0], DESIRED_HEADING[1],
+             color='blue', lw=1.5, label=r'$v_h^d$')
+    ax.set_title('Cos of angle between \nnew velocity (expected by inverse orca) and desired velocity')
+    ax.set_aspect('equal')
+    ax.legend(loc='upper right')
+    plt.colorbar(scatter)
+
+    fig, ax = plt.subplots()
+    scatter = ax.scatter(x=data[:, 2], y=data[:, 3],
+                         c=data[:, 9], cmap='viridis', vmin=VMIN, vmax=1.0)
     ax.arrow(HUMAN_POS[0], HUMAN_POS[1],
              HUMAN_HEADING[0], HUMAN_HEADING[1],
              color='red', lw=1.5, label=r'$v_h$')

policy/utils/get_velocity.py
@@ -50,14 +50,16 @@ class OptimalInfluence:
 
         self.u_hat = np.array(self.vo.right_tangent.normal)
         d = abs(self.u_hat.dot(current_to_desired))
+        # print(d)
         dmax = self.vo.find_dmax(self.velocity_circle, 'right')
         # print(dmax, d)
         d = min(dmax, d)
+        # print(dmax)
         self.u = d * self.u_hat
 
-        point = (self.vo.right_tangent.point[0] - self.u[0],
-                 self.vo.right_tangent.point[1] - self.u[1])
-        normal = self.vo.right_tangent.normal
+        tangent = self.vo.right_tangent
+        point = (tangent.point[0] - self.u[0], tangent.point[1] - self.u[1])
+        normal = tangent.normal
         proj_line = Tangent(point, normal)
         # There is no overlap between the relative velocity circle and
         # the line at a distance d from the tangent
@@ -77,10 +79,10 @@ class OptimalInfluence:
             # # If the intersection point is to the left of the normal,
             # # the minimum vB would be such that the relative velocity
             # # is at vA - dist_from_proj_line * u
-            side = self.vo.right_tangent.side(inter_point)
+            side = tangent.side(inter_point)
             if side < 0:
                 # print("right")
-                relative_velocity = np.array(self.vo.right_tangent.point) - d * self.u_hat
+                relative_velocity = np.array(tangent.point) - d * self.u_hat
                 self.vr = self.vh - relative_velocity
                 self.vh_new = tuple(np.array(self.vh) + self.collision_responsibility * self.u)
                 self.vr = tuple(self.vr)
@@ -119,13 +121,15 @@ class OptimalInfluence:
 
         self.u_hat = -np.array(self.vo.left_tangent.normal)
         d = abs(self.u_hat.dot(current_to_desired))
+        # print(d)
         dmax = self.vo.find_dmax(self.velocity_circle, 'left')
         d = min(dmax, d)
+        # print(dmax)
         self.u = d * self.u_hat
 
         tangent = self.vo.left_tangent
         point = (tangent.point[0] - self.u[0], tangent.point[1] - self.u[1])
-        normal = self.vo.left_tangent.normal
+        normal = tangent.normal
         proj_line = Tangent(point, normal)
         if not self.velocity_circle.line_overlap(proj_line):
             # print("No overlap of left projection line and velocity circle")
@@ -140,10 +144,10 @@ class OptimalInfluence:
             inter_point = center_line.intersect(proj_line)
             # dist_from_proj_line = proj_line.dist(self.vA)
 
-            side = self.vo.left_tangent.side(inter_point)
+            side = tangent.side(inter_point)
             if side < 0:
                 # print("right")
-                relative_velocity = np.array(self.vo.left_tangent.point) - d * self.u_hat
+                relative_velocity = np.array(tangent.point) - d * self.u_hat
                 self.vr = self.vh - relative_velocity
                 self.vh_new = tuple(np.array(self.vh) + self.collision_responsibility * self.u)
                 self.vr = tuple(self.vr)
@@ -296,19 +300,19 @@ class OptimalInfluence:
             angle_right = get_angle(self.vo.right_tangent.normal, current_to_desired)
 
             if 0 < angle_right < np.pi / 2:
-                # print("Projecting on right leg")
+                print("Projecting on right leg")
                 self.vr, self.u = self.handle_right_leg(current_to_desired)
                 if self.solution_exists:
                     return self.vr, self.u
 
             if np.pi/2 < angle_left < np.pi:
-                # print("Projecting on left leg")
+                print("Projecting on left leg")
                 self.vr, self.u = self.handle_left_leg(current_to_desired)
                 if self.solution_exists:
                     return self.vr, self.u
 
             if angle_right < 0 and angle_left < 0:
-                # print("Projecting on cutoff circle")
+                print("Projecting on cutoff circle")
                 return self.handle_cutoff_circle(current_to_desired)
 
             if self.solution_exists:

policy/utils/overlap_detection.py
@@ -251,18 +251,6 @@ class VelocityObstacle:
         point = self.cutoff_circle.center
         normal = np.array([m, -1.])
         normal /= norm(normal)
-
-        line = Tangent(point, normal)
-
-        if not velocity_circle.line_overlap(line):
-            # If there is no overlap between the center line and the velocity circle,
-            # dmax is the radius of the velocity circle - distance of center from tangent
-            return velocity_circle.radius - abs(np.dot(chosen_normal, velocity_circle.center))
-
-        point = self.cutoff_circle.center
-        normal = np.array([m, -1.])
-        normal /= norm(normal)
-
         line = Tangent(point, normal)
 
         if not velocity_circle.line_overlap(line):
@@ -287,9 +275,10 @@ class VelocityObstacle:
         point2 = (x2, y2)
 
         cross1 = chosen_normal[0] * y1 - chosen_normal[1] * x1
-        cross2 = chosen_normal[0] * y2 - chosen_normal[0] * x2
+        cross2 = chosen_normal[0] * y2 - chosen_normal[1] * x2
 
         if cross1 > 0 and cross2 > 0:
+            # print("Both chosen")
             # Both points to the left of the normal at origin
             if norm(point1) > norm(point2):
                 return abs(np.dot(chosen_normal, point1))
@@ -297,9 +286,11 @@ class VelocityObstacle:
             return abs(np.dot(chosen_normal, point2))
 
         if cross1 > 0:
+            # print("1 chosen")
             return abs(np.dot(chosen_normal, point1))
 
         if cross2 > 0:
+            # print("2 chosen")
             return abs(np.dot(chosen_normal, point2))
 
         # print("Both intersection points are to the right of the normal. Impossibe case...")